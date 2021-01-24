18a11ed1b6f8485c15a1a2914e924b2cfa546c70
Tuan Anh <anhmeorv99@gmail.com>
2021-01-24 21:39:00 +0700

    rels

--- Control/test.c
+++ Control/test.c
@@ -1,8 +1,27 @@
 #include "../symtab/object_data.h"
 #include "../symtab/jsonapi.h"
+#include <ctype.h>
 #include <stdio.h>
 
-int main(){
-    printf("size : %ld ", sizeof(Data_base));
+int isNumber(char*numbers)
+{
+    int i, correctNum = 1;
+    if (strlen(numbers) == 0) return 0;
+    for(i = 0 ; i < strlen(numbers) ; ++i)
+    {
+        if(!isdigit(numbers[i]))
+        {
+            correctNum = 0;
+            break;
+        }
+    }
+
+    if(correctNum == 0) return 0;
+    return 1;
+}
+int main()
+{
+    int a = isNumber("");
+    printf("%d", a);
     return 1;
 }
